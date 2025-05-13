61a193aa3044bc9376f75437bcbb2580bfec8327
NikolaiHoretski <nikolai.horetski@gmail.com>
2025-05-13 18:00:24 +0300

    add polygon_base, poligon_plugins

---
+++ src/polygon_base/include/polygon_base/regular_polyhon.hpp
@@ -0,0 +1,17 @@
+#ifdef POLYGON_BASE_REGULAR_POLYGON_HPP
+#define POLYGON_BASE_REGULAR_POLYGON_HPP
+
+namespase polygon_base {
+
+class RegularPolygon {
+    public:
+    virtual void initialize(double side_length) = 0;
+    virtual double area() = 0;
+    virtual ~RegularPolygon(){}
+
+    protected:
+    RegularPolygon(){}
+};
+}
+
+#endif
---
+++ src/polygon_plugins/src/polygon_plugins.cpp
@@ -0,0 +1,14 @@
+#include <polygon_base/regular_polygon.hpp>
+
+namespace polygon_plugins
+{
+
+PolygonPlugins::PolygonPlugins()
+{
+}
+
+PolygonPlugins::~PolygonPlugins()
+{
+}
+
+}  // namespace polygon_plugins
